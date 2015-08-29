3c8cae67391e4407690f8f474c43af3c3e342a8e
Nikolai Bezruk <kolima@ukr.net>
2015-08-29 15:59:15 +0300

    add(prices): init

---
+++ public/modules/prices/config/prices.client.routes.js
@@ -0,0 +1,25 @@
+'use strict';
+
+class PricesRoutes {
+    constructor($stateProvider) {
+        this.$stateProvider = $stateProvider;
+        this.init();
+    }
+
+    init() {
+        this.$stateProvider
+            .state('prices', {
+                url: '/prices',
+                controller: 'PricesController',
+                controllerAs: 'vm',
+                templateUrl: '/modules/prices/views/list-prices.client.view.html'
+            });
+    }
+
+    // @ngInject
+    static factory($stateProvider){
+        return new PricesRoutes($stateProvider);
+    }
+}
+
+angular.module('prices').config(PricesRoutes.factory);
---
+++ public/modules/prices/controllers/prices.client.controller.js
@@ -0,0 +1,9 @@
+'use strict';
+
+class PricesController {
+    // @ngInject
+    constructor() {
+    }
+}
+
+angular.module('prices').controller('PricesController', PricesController);
